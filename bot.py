a3b3ccd2b9989b88f35095cca8a3a291248accfc
Army-R <80074550+Army-R@users.noreply.github.com>
2022-03-20 15:20:23 -0600

    Rename from chatbot.py to bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 22 18:40:29 2022
-
-@author: Army-R
-"""
 from chatterbot import ChatBot
 #from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
 
 # ChatBot name
-chatbot = ChatBot("Pal")
+bot = ChatBot("Pal")
 
 conversation = open("chatbot_training.txt", "r").readlines()
 
-trainer = ListTrainer(chatbot)
+trainer = ListTrainer(bot)
 #trainer = ChatterBotCorpusTrainer(chatbot)
 
 trainer.train(conversation)
@@ -24,7 +17,7 @@ trainer.train(conversation)
 while True:
     try:
        request = input('Me: ')
-       aswr = chatbot.get_response(request)
+       aswr = bot.get_response(request)
        print("Andrea", aswr)
 
     except(KeyboardInterrupt, EOFError, SystemExit):
